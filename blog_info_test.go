a4ba7512c44d37702c8bd6840351d36cad877968
UshioShugo <ushio.s@gmail.com>
2016-05-25 00:37:36 +0900

    change response var name.

--- blog_info_test.go
+++ blog_info_test.go
@@ -6,13 +6,13 @@ func TestBlogInfo(t *testing.T) {
 	test := func(bi string, ak string) {
 		c := NewClient(testAPIKey1)
 
-		bir, err := c.BlogInfo(testBlogIdentifier)
+		res, err := c.BlogInfo(testBlogIdentifier)
 		if err != nil {
 			t.Errorf("BlogInfo got error: %s", err)
 		}
 
-		if bir.Meta.Status != 200 {
-			t.Errorf("blog-info status expected(%d) but (%d) ", 200, bir.Meta.Status)
+		if res.Meta.Status != 200 {
+			t.Errorf("blog-info status expected(%d) but (%d) ", 200, res.Meta.Status)
 		}
 	}
 
