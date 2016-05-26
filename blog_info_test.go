af4d289fabd7361039216f4c7e65a6d710708e3b
UshioShugo <ushio.s@gmail.com>
2016-05-27 01:06:41 +0900

    add blog info name test.

--- blog_info_test.go
+++ blog_info_test.go
@@ -3,7 +3,7 @@ package tumblgo
 import "testing"
 
 func TestBlogInfo(t *testing.T) {
-	test := func(bi string, ak string) {
+	test := func(bi string, ak string, name string) {
 		c := NewClient(testAPIKey1)
 
 		res, err := c.BlogInfo(testBlogIdentifier)
@@ -14,7 +14,11 @@ func TestBlogInfo(t *testing.T) {
 		if res.Meta.Status != 200 {
 			t.Errorf("blog-info status expected(%d) but (%d) ", 200, res.Meta.Status)
 		}
+
+		if res.Response.Blog.Name != name {
+			t.Errorf("blog-info name expected (%s) but (%s)", name, res.Response.Blog.Name)
+		}
 	}
 
-	test(testBlogIdentifier, testAPIKey1)
+	test(testBlogIdentifier, testAPIKey1, "scipsy")
 }
